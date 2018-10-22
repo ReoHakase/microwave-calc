467f723aad949c7c2c10fe3b035e92c216323777
ReonaldHKT <reonaldhkt+webs@gmail.com>
2018-10-22 20:24:12 +0900

    Add manifest.json and change something

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const glob = require('glob');
 const minifyPlugin = require("babel-minify-webpack-plugin");
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin')
 
 let PROD;
 process.env.NODE_ENV == 'production' ? PROD = true : PROD = false;
@@ -27,7 +28,19 @@ module.exports = {
     }),
     new htmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html')
-    })
+    }),
+    new copyWebpackPlugin([
+      {
+        from: path.join(__dirname, 'src', 'manifest.json'),
+        to: path.join(__dirname, 'public', 'manifest.json'),
+        toType: 'file'
+      },
+      {
+        from: path.join(__dirname, 'src', 'icon', 'icon192.png'),
+        to: path.join(__dirname, 'public', 'icon', 'icon192.png'),
+        toType: 'file'
+      }
+    ], {})
   ],
   module: {
     rules: [
